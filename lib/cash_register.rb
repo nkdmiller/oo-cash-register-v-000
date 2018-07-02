49bff26a1785b276ce74d3318781187a988d07d3
Nate Miller <natekmiller96@gmail.com>
2018-07-02 20:09:03 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,8 +6,8 @@ class CashRegister
     @total = 0
   end
 
-  def add_item(title, price)
-    @total += price
+  def add_item(title, price, quantity = 1)
+    @total += price * quantity
   end
 
 end
